1f1d05f52c610ee93e32a0fd95f883a0a50973d5
Pedro Vicente <pedrozwpiper@gmail.com>
2017-06-22 21:40:19 -0300

    Primeiro commit

---
+++ aula04/tipos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Tipos</title>
+
+	<link rel="stylesheet" href="css/style.css"/>
+</head>
+<body>
+
+<div class="estilo">
+	<?php
+		$nome = "Pedro Vicente";
+		$idade = 19;
+		echo "$nome tem $idade anos";
+	?>
+</div>
+
+</body>
+</html>
---
+++ ola.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Primeiro exemplo php</title>
+	</head>
+
+<body>
+	<h1> Testando PHP </h1>
+	<?php
+		echo "Ola, Mundo!";
+	?>
+</body>
+</html
